a3eb80c0d8061ac026827dee202741951962c58e
Benja Aguilar <benjaaguilar02@gmail.com>
2026-06-18 17:29:24 -0300

    feat: add index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const Mate = mongoose.model(
+  "Mate",
+  new mongoose.Schema({
+    name: String,
+    price: Number,
+  }),
+);
+
+const DB_URL = "mongodb://color:password@db:27017/backend?authSource=admin";
+mongoose.connect(DB_URL);
+
+const app = express();
+
+app.get("/", async (_req, res) => {
+  console.log(`GET request of "/"`);
+  console.log("Hello im hot reload on docker");
+  const mates = await Mate.find();
+
+  res.json({
+    success: true,
+    message: "welcome to my app",
+    mates,
+  });
+});
+
+app.get("/add", async (_req, res) => {
+  console.log(`GET request of "/add"`);
+  await Mate.create({ name: "porongo", price: 10 });
+
+  res.json({
+    success: true,
+    message: "Mate created",
+  });
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
